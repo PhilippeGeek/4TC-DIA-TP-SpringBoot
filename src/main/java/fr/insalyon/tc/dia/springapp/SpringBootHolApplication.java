c3448ce40fea91efa8878de2b9ee0de34c625e16
Philippe Vienne <philippegeek@gmail.com>
2017-03-16 10:47:54 +0100

    Add health

--- src/main/java/fr/insalyon/tc/dia/springapp/SpringBootHolApplication.java
+++ src/main/java/fr/insalyon/tc/dia/springapp/SpringBootHolApplication.java
@@ -1,12 +1,25 @@
 package fr.insalyon.tc.dia.springapp;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class SpringBootHolApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootHolApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootHolApplication.class, args);
+    }
+
+    @Bean
+    public HealthIndicator authServiceHealthIndicator() {
+        return () -> {
+            return Health.up().build();
+        };
+    }
 }
