6d76811f6e6d639ea0de5b170a387eaa69b59b6f
Philippe Vienne <philippegeek@gmail.com>
2017-03-16 09:32:59 +0100

    Initial commit

---
+++ src/main/java/fr/insalyon/tc/dia/springapp/SpringBootHolApplication.java
@@ -0,0 +1,12 @@
+package fr.insalyon.tc.dia.springapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHolApplication.class, args);
+	}
+}
